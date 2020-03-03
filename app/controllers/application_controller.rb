4a553d7e7d6b645da5b9d61252e475047a5ce9ae
Christoph Willisch <christoph.j.willisch@gmail.com>
2020-03-03 16:56:18 +0100

    model view controller for application created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def new
+    @application = Application.new
+  end
+
+  def create
+    @application = Application.new(letter_of_motivation: params["application"]["letter_of_motivation"])
+    @application.save
+    redirect_to root_path
+  end
 end
+
