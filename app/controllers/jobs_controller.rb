99a2494e8fa1b70712dffe49182d9e8b0b10cf6a
Carolin Janssen <c.j@hempberg.com>
2020-03-04 18:36:10 +0100

    changed search function things

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,26 +1,29 @@
 class JobsController < ApplicationController
 
   def index
-    @job_name_value = params[:job_name]
-    @job_cities_value = jobs_params[:city]
-    @job_categories_value = jobs_params[:department]
     @jobs = Job.all
     @job_cities = []
+    @job_categories = []
+    if params[:query].present?
+      @job_name_value = params[:job_name]
+      @job_cities_value = jobs_params[:city]
+      @job_categories_value = jobs_params[:department]
+
+
+      @jobs = Job.all
+      @jobs = @jobs.where(category: jobs_params[:department]) if jobs_params[:department].present?
+      @jobs = @jobs.where(location: jobs_params[:city]) if jobs_params[:city].present?
+      @jobs = @jobs.where("title ILIKE ?", "%#{params[:job_name]}%") if params[:job_name].present?
+    end
+
     Job.all.each do |job|
       @job_cities << job.location
     end
     @job_cities = @job_cities.uniq
-
-    @job_categories = []
     Job.all.each do |job|
       @job_categories << job.category
     end
     @job_categories = @job_categories.uniq
-
-    @jobs = Job.all
-    @jobs = @jobs.where(category: jobs_params[:department]) if jobs_params[:department].present?
-    @jobs = @jobs.where(location: jobs_params[:city]) if jobs_params[:city].present?
-    @jobs = @jobs.where("title ILIKE ?", "%#{params[:job_name]}%") if params[:job_name].present?
   end
 
   def show
