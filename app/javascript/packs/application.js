969692b9a742186830f576220790ca6618e216e5
Christoph Willisch <christoph.j.willisch@gmail.com>
2020-03-05 13:07:06 +0100

    sweetalert created

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert({
+  title: "You successfully applied!",
+  text: "",
+  icon: "success"
+}, (value) => {
+  console.log(value);
+});
