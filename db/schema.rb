3a9c5e1428802e376c2af8db9b0b837cbb94a3d6
David Wise <davidmarkwise@gmail.com>
2020-03-10 13:32:49 +0100

    adds applicant name to progress dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_142324) do
+ActiveRecord::Schema.define(version: 2020_03_09_110747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_142324) do
     t.text "address"
     t.float "grade_point_average"
     t.string "highest_education_attained"
-    t.boolean "admin", default: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
