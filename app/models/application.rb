b7b8f3ac718fb77d5c8e1373e5f2146d26b535ff
Jean Hugues Kabuiku <amazinggaijin@gmail.com>
2020-03-10 10:37:34 +0100

    feature to change the status done

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
-  STATUSES = ["Phone Screening", "Email-based Case Study", "On-site interview", "Final Decision" ]
+  STATUSES = ["Phone screening", "Email-based case study", "On-site interview", "Final decision" ]
 
   DESCRIPTIONS = ['You will be contacted by managerial staff, who will review with you your qualifications and discuss your expectations for the position, as well as your career aspirations.', 'To further test your qualifications, you will be asked a series of written questions, to be returned to us via email within 3 to 5 days.', 'You will be invited to an on-site meeting with one or more members of our managerial staff, in order to further assess your skill and abilities.','Awaiting final decision on your application.']
 
@@ -12,9 +12,6 @@ class Application < ApplicationRecord
 
   enum status: STATUSES
 
-  def status_description
-    index = STATUSES.index(status)
-    return DESCRIPTIONS[index]
-  end
+
 
 end
