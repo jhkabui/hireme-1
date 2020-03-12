f14084d4380c710c8f0a74d82583f553877ce94c
Carolin Janssen <c.j@hempberg.com>
2020-03-12 10:40:27 +0100

    changes routes, color

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     get 'users', :to => 'profiles#show', :as => :user_root # Rails 3
   end
 
-  root to: 'pages#home'
+  root to: 'pages#hireme'
 
   resources :jobs, only: [:index, :show] do
     resources :user_jobs, only: [:create, :show, :destroy]
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/progress", to: "pages#dashboard", as: "dashboard"
   get "/profile/", to: "profiles#show"
-  get "/hireme/", to: "pages#hireme"
+  get "/lewagon/", to: "pages#home"
 
 end
